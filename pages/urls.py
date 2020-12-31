a166b018ba391da4941a24b78f59cc8e809305f4
Subham Singh <subham.singh@hackerearth.com>
2020-12-31 07:31:34 +0530

    add pages app with basic templates and tests

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import home_page_view
+from .views import HomePageView
 
 urlpatterns = [
-    path('', home_page_view, name='home'),
+    path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse("Hello World")
+class HomePageView(TemplateView):
+    template_name = 'home.html'
