3e7ca01ef2fceb05a9603d993442ea1ac435d56f
Subham Singh <subham.singh@hackerearth.com>
2020-12-31 05:57:06 +0530

    users: add custom user model and migrations and tests dockerfile: add volumes for data persistence

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'users.CustomUser'
