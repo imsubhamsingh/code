0b9b5b861dd7bc3c2b4fa4cb80a6a0dafb71361c
Subham Singh <subham.singh@hackerearth.com>
2020-12-31 21:16:26 +0530

    users: add unittest for signup template

--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
-class CustomUserTest(TestCase):
+class CustomUserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
@@ -24,3 +25,15 @@ class CustomUserTest(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
