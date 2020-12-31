dae1d556e198a96e2c30393d24c06b34788b7164
Subham Singh <subham.singh@hackerearth.com>
2020-12-31 21:08:08 +0530

    users: add signup flow in user registration

--- hello_project/urls.py
+++ hello_project/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # local apps
     path("", include("pages.urls")),
+    path("accounts/", include("users.urls")),
 ]
